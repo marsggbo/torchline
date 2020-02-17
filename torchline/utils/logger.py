f1e5eb6f5d6f640abd60f367a0c95bceae2a8834
marsggbo <1435679023@qq.com>
2020-02-17 22:13:21 +0800

    fix logger bug

--- torchline/utils/logger.py
+++ torchline/utils/logger.py
@@ -11,50 +11,34 @@ class Logger(object):
     封装后的logging
     '''
 
-    def __init__(self, logger_name=None):
+    def __init__(self, logger_name=None, cfg=None):
         '''
             指定保存日志的文件路径，日志级别，以及调用文件
             将日志存入到指定的文件中
         '''
 
-        log_name = 'log.txt'
+        log_name = 'log.txt' if cfg is None else cfg.logger.log_file
         # 创建一个logger
+        file = cfg.logger.log_file
         self.logger = logging.getLogger(logger_name)
-        self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.INFO)
+        self.logger.propagate = False
+        if (self.logger.hasHandlers()):
+            self.logger.handlers.clear()
+        formatter = logging.Formatter('[%(asctime)s] %(filename)s->%(funcName)s line:%(lineno)d [%(levelname)s]%(message)s')
 
-        # 创建一个handler，用于写入日志文件
-        self.log_time = time.strftime("%Y_%m_%d")
-        self.log_dir = './'
-        file_dir = os.path.join(os.getcwd(), self.log_dir)
-        if not os.path.exists(file_dir):
-            os.mkdir(file_dir)
-        self.log_path = file_dir
-        self.log_name = os.path.join(self.log_path, log_name)
+        hdlr = logging.FileHandler(file, 'a', encoding='utf-8')
+        hdlr.setLevel(logging.INFO)
+        hdlr.setFormatter(formatter)
+        self.logger.addHandler(hdlr)
 
-        # fh = logging.FileHandler(self.log_name, 'a')  # 追加模式  这个是python2的
-        fh = logging.FileHandler(self.log_name, 'a', encoding='utf-8')  # 这个是python3的
-        fh.setLevel(logging.INFO)
+        strhdlr = logging.StreamHandler()
+        strhdlr.setLevel(logging.INFO)
+        strhdlr.setFormatter(formatter)
+        self.logger.addHandler(strhdlr)
 
-        # 再创建一个handler，用于输出到控制台
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
-
-        # 定义handler的输出格式
-        formatter = logging.Formatter(
-            '[%(asctime)s] %(filename)s -> %(funcName)s line:%(lineno)d [%(levelname)s] %(message)s')
-        fh.setFormatter(formatter)
-        ch.setFormatter(formatter)
-
-        # 给logger添加handler
-        self.logger.addHandler(fh)
-        self.logger.addHandler(ch)
-
-        #  添加下面一句，在记录日志之后移除句柄
-        # self.logger.removeHandler(ch)
-        # self.logger.removeHandler(fh)
-        # 关闭打开的文件
-        fh.close()
-        ch.close()
+        hdlr.close()
+        strhdlr.close()
 
     def getlog(self):
         return self.logger
