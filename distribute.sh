0b494c699f6703b5995c4bc87a6d8a72d61abe73
marsggbo <1435679023@qq.com>
2020-03-03 17:28:09 +0800

    update distribute.sh

--- distribute.sh
+++ distribute.sh
@@ -1,4 +1,9 @@
 rm -rf build dist torchline.egg-info
 python setup.py sdist bdist_wheel
-python -m twine upload dist/*
-#python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+if (($1==1));then
+    python -m twine upload dist/*
+elif (($1==2));then
+    python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+else
+    echo "Wrong command, only support 1 or 2"
+fi
